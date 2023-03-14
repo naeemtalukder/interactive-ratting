c510d9328e2b436ccc777911c35773d653a786e9
naeemtalukder <naeemtalukder169135@gmail.com>
2023-03-14 23:03:48 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import { StyledButton, StyledButtonWithHover } from './component/styled/Button';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='button_box'>
+      <StyledButton>Live</StyledButton>
+      <StyledButton variant='primary'>Github</StyledButton>
+     
+     </div>
+     <div >
+     <StyledButtonWithHover>Hover Me</StyledButtonWithHover>
+     </div>
     </div>
   );
 }
